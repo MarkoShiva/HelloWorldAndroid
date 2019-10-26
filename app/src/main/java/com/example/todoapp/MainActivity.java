d21bf1e714fc559181842ea24f1811a340e5d0f0
Marko in1t3r Pavlovic <marko.shiva.pavlovic@gmail.com>
2019-10-26 15:23:51 +0200

    Empty message case

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.todoapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.todoapp.MESSAGE";
@@ -17,14 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    /** Called when the user taps the Sebd button*/
+    /** Called when the user taps the Send button*/
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void sendMessage(View view) {
         // Do something in the response to button click
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
 
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        if (Objects.equals(message, "")){
+            Toast.makeText(this, "You need to enter a message.", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(this, DisplayMessageActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }
     }
 }
